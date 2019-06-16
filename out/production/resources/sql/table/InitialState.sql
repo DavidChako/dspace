04cdc27ba45d54c3ccaceb13d6179ab521b7a593
David Chako <davidchako@Davids-iMac.home>
2019-06-16 14:38:10 -0400

    Fixed .gitignore to ignore out directory

--- out/production/resources/sql/table/InitialState.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists Tetray (
-    w bigint not null,
-    x bigint not null,
-    y bigint not null,
-    z bigint not null
-)
--- out/production/resources/sql/table/Tetray.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS Tetray (
-    w bigint not null,
-    x bigint not null,
-    y bigint not null,
-    z bigint not null
-);
